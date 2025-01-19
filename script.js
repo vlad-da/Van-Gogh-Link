abca09e456d0701d20ada0476f3cac4a064812a2
Vlasenko Vladislav <vl.vlasenko@mail.astondevs.ru>
2025-01-20 01:50:42 +0300

    Добавляет шрифты и вносит правки

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const burger = document.querySelector('.header-nav__menu');
+const burger = document.querySelector('.header-nav__menu'),
+        menu = document.querySelector('.burger-menu');
 
 burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
+    burger.classList.toggle('active');
+    menu.classList.toggle('open');
 })
