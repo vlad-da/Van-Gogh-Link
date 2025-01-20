92e7d00c9cc115139f9cc85cd7c76001110063d2
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2025-01-20 18:58:26 +0300

    Добавляет шрифты

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const burger = document.querySelector('.header-nav__menu'),
-        menu = document.querySelector('.burger-menu');
+        menu = document.querySelector('.burger-menu'),
+        header = document.querySelector('header');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     menu.classList.toggle('open');
+});
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50){
+       header.classList.add('shadow')
+     } else {
+        header.classList.remove('shadow')
+     }
 })
